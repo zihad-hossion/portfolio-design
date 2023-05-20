2931aa93d2407ab1203dd9ae9002e7cd8f74bec7
zihadhossion <zihad41h@gmail.com>
2023-05-20 18:09:46 +0600

    header finish

--- js/main.js
+++ js/main.js
@@ -1,7 +1,19 @@
-//sidebar show or hide
+const navBar = document.querySelector("#navbar");
 const menuBtn = document.querySelector("#nav-btn");
 const closeBtn = document.querySelector("#close-btn");
 const sideBar = document.querySelector("#sidebar");
+
+// add fixed class to navbar
+window.addEventListener("scroll", function () {
+    if (this.window.pageYOffset > 80) {
+        navBar.classList.add("navbar-fixed");
+    } else {
+        navBar.classList.remove("navbar-fixed");
+    }
+});
+
+//sidebar show or hide
+
 
 menuBtn.addEventListener("click", function () {
     sideBar.classList.add("show-sidebar");
